2fc9a0142969ae10009078565f34f4f6ace7c9b4
Diana-Matu <112359437+Diana-Matu@users.noreply.github.com>
2024-10-18 16:53:28 -0600

    Update parser.clj
    Archivo con parser de ae y wae

--- src/parser.clj
+++ src/parser.clj
@@ -2,11 +2,54 @@
   (:require [grammars :as wae]))
 
 (defn parser-AE
-    "Por implementar"
-    [exp]
-    ())
+  "Parser de expresiones aritméticas"
+  [exp]
+  (cond
+    ;; Si es un número
+    (number? exp) 
+    (wae/numG exp)
+    
+    ;; Si es una suma
+    (and (list? exp) (= '+ (first exp)))
+    (wae/addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+    
+    ;; Si es una resta
+    (and (list? exp) (= '- (first exp)))
+    (wae/subG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+
+    ;; Cualquier otra cosa
+    :else 
+    (throw (IllegalArgumentException. "Expresión AE no válida"))))
+
 
 (defn parser-WAE
-    "Por implemntar"
-    [exp]
-    ())
+  "Parser de expresiones WAE"
+  [exp]
+  (cond
+    ;; Si es un número
+    (number? exp) 
+    (wae/numG exp)
+    
+    ;; Si es un identificador
+    (symbol? exp)
+    (wae/idG exp)
+    
+    ;; Si es una expresión `with`
+    (and (list? exp) (= 'with (first exp)))
+    (let [bindings-list (second exp)
+          var (first bindings-list)
+          val (second bindings-list)
+          body (nth exp 2)]
+      (wae/withG (wae/bindings var (parser-WAE val)) (parser-WAE body)))
+    
+    ;; Si es una suma
+    (and (list? exp) (= '+ (first exp)))
+    (wae/addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+    
+    ;; Si es una resta
+    (and (list? exp) (= '- (first exp)))
+    (wae/subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+
+    ;;  Cualquier otra cosa 
+    :else
+    (throw (IllegalArgumentException. "Expresión WAE no válida"))))
